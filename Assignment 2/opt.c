63c96c90ac5997cd45332eea512c0377186140c0
KostyaIliouk <kostya.iliouk@hotmail.com>
2019-07-07 15:23:41 -0400

    started to work on opt

--- Assignment 2/opt.c
+++ Assignment 2/opt.c
@@ -12,6 +12,9 @@ extern int debug;
 
 extern struct frame *coremap;
 
+int lineCtr = 0;
+
+int *coremapNextUse;
 /* Page to evict is chosen using the optimal (aka MIN) algorithm. 
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
@@ -26,7 +29,15 @@ int opt_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void opt_ref(pgtbl_entry_t *p) {
+	// go through the coremap and minus one from their next access
+	for(int i = 0; i < memsize; i++){
+		coremap[i].pte->nextOccurence--;
+	}
+	// find next occurence for p
+	// open ref string file at line lineCtr, keep going until same vaddr is found
 
+	// new line in ref string
+	lineCtr++;
 	return;
 }
 
@@ -34,6 +45,6 @@ void opt_ref(pgtbl_entry_t *p) {
  * replacement algorithm.
  */
 void opt_init() {
-
+	coremapNextUse = malloc(sizeof(int) * memsize);
 }
 
