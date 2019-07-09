d47bf36facadfb1a35871c92025cc2428979e8ab
KostyaIliouk <kostya.iliouk@hotmail.com>
2019-07-08 20:14:00 -0400

    trying to debug opt

--- Assignment 2/opt.c
+++ Assignment 2/opt.c
@@ -9,9 +9,11 @@ extern int memsize;
 
 extern int debug;
 
-extern char* tracefile;
-
 extern struct frame *coremap;
+
+// imported globals and lineCtr for the tracefile
+
+extern char* tracefile;
 
 int lineCtr;
 
@@ -46,17 +48,20 @@ void opt_ref(pgtbl_entry_t *p) {
 	char buf[MAXLINE];
 	struct frame *findOccurence;
 	// find p in the coremap
+	printf("going into finding relative frame");
 	for (int i = 0; i < memsize; i++) {
 		if (coremap[i].pte->frame == p->frame) {
 			findOccurence = &coremap[i];
 		}
 	}
+	printf("frame was found");
 	// go through the coremap and minus one from their next access
 	for(int i = 0; i < memsize; i++){
 		if (!coremap[i].in_use){
 			coremap[i].nextOccurence--;
 		}
 	}
+	printf("minus one from exisiting frame occurences");
 	// find next occurence for p
 	// open ref string file at line lineCtr, keep going until same vaddr is found
 	if(tracefile!= NULL) {
@@ -65,6 +70,7 @@ void opt_ref(pgtbl_entry_t *p) {
 			exit(1);
 		}
 	}
+	printf("opened trace file");
 	findOccurence->nextOccurence = -1;
 	int currentLine = 0;
 	addr_t pVaddr = 0;
@@ -72,12 +78,14 @@ void opt_ref(pgtbl_entry_t *p) {
 	char type;
 	while(fgets(buf, MAXLINE, tfp) != NULL) {
 		if(buf[0] != '='){
-			if (currentLine == lineCtr){
+			if (currentLine == lineCtr){ 
 				sscanf(buf, "%c %lx", &type, &pVaddr);
+				printf("found current line");
 			} else if (currentLine > lineCtr){
 				sscanf(buf, "%c %lx", &type, &cVaddr);
 				if	(pVaddr == cVaddr) {
 					findOccurence->nextOccurence = currentLine - lineCtr;
+					printf("found next occurence");
 					break;
 				}
 			}
